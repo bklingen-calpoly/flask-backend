f91ca07b6ede303b567aed29619457f2986e4d8f
BJ Klingenberg <75038922+bklingen-calpoly@users.noreply.github.com>
2021-01-11 12:45:08 -0800

    first step linking fe-be

--- sample_backend.py
+++ sample_backend.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import json
+# for linking frontend-backend
+from flask_cors import CORS
+
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
